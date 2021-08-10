f4e7b5fc9376cc0068c7dbf6632ca0f6928b988d
OctoidGames <87039261+OctoidGames@users.noreply.github.com>
2021-08-10 17:33:45 -0400

    Add files via upload

---
+++ Dep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install ffmpeg
+sudo apt-get install python3
+sudo atp-get install python3-pip 
+pip install pytube
+pip install youtube_title_parse
+pip install moviepy
+pip install pydub
+
