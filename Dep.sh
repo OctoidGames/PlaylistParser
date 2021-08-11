d2fc44babdf308ecfe5d8ceccd1d5b8d6ba23d07
OctoidGames <87039261+OctoidGames@users.noreply.github.com>
2021-08-10 21:16:44 -0400

    Update Dep.sh

--- Dep.sh
+++ Dep.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install ffmpeg
 sudo apt-get install python3
 sudo atp-get install python3-pip 
+sudo apt-get install python3-tk
 pip install pytube
 pip install youtube_title_parse
 pip install moviepy
